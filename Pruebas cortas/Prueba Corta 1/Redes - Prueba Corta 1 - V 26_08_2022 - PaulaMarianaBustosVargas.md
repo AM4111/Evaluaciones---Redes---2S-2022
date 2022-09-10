78a734fc93ff49b96e293db62c84c1a3b629610d
AM4111 <pauma1018@gmail.com>
2022-09-10 03:54:09 -0600

    Prueba corta 2-3

---
+++ Pruebas cortas/Prueba Corta 1/Redes - Prueba Corta 1 - V 26_08_2022 - PaulaMarianaBustosVargas.md
@@ -0,0 +1,47 @@
+Tecnológico de Costa Rica<br> 
+Escuela de Ingeniería en Computación<br> 
+IC: 7602-Redes  - 2 Semestre 2022 <br> 
+2018093728 - Paula Mariana Bustos Vargas
+____
+<center> <h1> Prueba Corta 1 - V 26-08-2022 </h1> </center> 
+
+**1. Explique de forma concisa, ¿Porqué la transmisión de ondas de baja frecuencia no es práctica en medios inalámbricos?**
+
+Porque se pueden ver afectadas por otras trasmisiones, tambien pueden darse la colision de datos , ademas de que se atenuan más facilmente. Pasa-Banda que es el metodo de trasmision inalambrico, no es recomendado el uso de ondas de baja frecuencia como en el Wirless
+____
+**2. Ante una crisis como conflicto bélico o un desastre natural, que daña o afecta los canales de comunicación o infraestructura de un país, ¿Qué tipo de transmisión recomendaría a ese país para mantener comunicación con el resto del mundo? Explique.**
+
+
+Recomendaria la trasmision por medio de satelites GEO (Satélites geoestacionarios) Debido a que son especiales para zonas de conflicto y :
+1. Ya fueron utilizada para este fin
+2. Dificiles de bloquear
+3. No rastreables: No se sabria la ubicacion por la cual se esta trasmitiendo
+4. Aunque es extremadamente lento son más confiables y seguros que trasmitir por otros medios 
+
+____
+**3. ¿En que consiste el concepto ancho de banda en telecomunicaciones? Explique detalladamente**
+
+El rango/cantidad de frecuencia de 0 a fc que se pueden trasmitir por un medio sin que se atenue.  Es una propiedad física del medio de transmisión y por lo general depende de la construcción, grosor y longitud de dicho medio 
+
+fc = frecuencia de corte : que es donde la frecuencia se atenua, se considera que una caida del potencia a la mitad
+
+Los canales son porciones de espectro del ancho de banda 
+____
+**4. ¿Por cuáles motivos se presenta la atenuación en una señal? Explicar tanto en medios cableados como no cableado**
+
+La atenuacion de una señal se puede ver afectado por diversas situacion como alguna Complicacion con el medio, la distancia, la potencia que lleva la señal, la interferencia de otras señales. En cuestiones de los medios 
+
+Medio cableados: 
+- Cuestiones climatologicas
+- Eventos Naturales
+- Ondas electromagneticas, como las del microondas
+
+La atenuacion en fibra optica es menor que otros medio cableados.
+
+Medios no cableados:
+- Paso/penetracion de objetos/obstaculos
+- Cuestiones climatologicas
+- Interferencia
+- Eventos Naturales
+- Saturacion de datos
+
---
+++ Pruebas cortas/Prueba Corta 2-3/Redes - Prueba Corta 2 - V 09_09_2022 - PaulaMarianaBustosVargas.c
@@ -0,0 +1,294 @@
+#include <stdio.h>
+#include <math.h>
+
+//Exponente pero entero
+//https://www.delftstack.com/es/howto/c/exponents-in-c/#:~:text=exponenciaci%C3%B3n%20en%20C.-,Usar%20pow%20como%20funci%C3%B3n%20de%20exponenciaci%C3%B3n%20en%20C,de%20herramientas%20del%20compilador%20gcc%20.
+int exponentInt(const int base, int n){
+    int i, p = base;
+    for (i = 1; i < n-1; ++i)
+        p *= base;
+    return p;
+}
+
+// Va posicionando los valores que entran en su respectiva posicion del haming
+// Los primero 7 bits (7,6,5,4,3,2,1) Posiciones en hamming (3,5,6,7,9,10,11)
+// BIT -> HAMMING
+// (7->3),(6->5),(5->6),(4->7),(3->9),(2->10),(1->11)
+int hamming_CalBits(int var) {
+
+	int bit1=0;
+	int bit2=0;
+	int bit4=0;
+	int bit8=0;
+	int hamming=0;
+
+	//printf("hamming %d\n",hamming);
+	//printf("var&1 == 1 %d\n",var&1);
+	if ((var&1) == 1){ // hace una mascara para saber si el bit 1 esta encendido posicion 11 hamming 
+		bit1+=1;		//influye en el bit de pariedad 1
+		bit2+=1;		//influye en el bit de pariedad 2
+		bit8+=1;		//influye en el bit de pariedad 8
+		hamming += 1;
+		//printf("Entre a bit 1\n");
+	}
+
+	//printf("------------------------------------- \n");
+	//printf("hamming %d\n",hamming);
+	//printf("var&2 == 2 -> %d\n",var&2);
+	//printf("var-> %d\n",var);
+	if ((var&2) == 2){ // hace una mascara para saber si el bit 2 esta encendido posicion 10 hamming
+		bit2+=1;		//influye en el bit de pariedad 2
+		bit8+=1;		//influye en el bit de pariedad 8
+		hamming += 2;
+		//printf("Entre a bit 2\n");
+	}
+
+	//printf("------------------------------------- \n");
+	//printf("hamming %d\n",hamming);
+	//printf("var&4 == 4 -> %d\n",var&4);
+	if ((var&4) == 4){ // hace una mascara para saber si el bit 3 esta encendido posicion 9 hamming
+		bit1+=1;		//influye en el bit de pariedad 1
+		bit8+=1;		//influye en el bit de pariedad 8
+		hamming += 4;
+		//printf("Entre a bit 3\n");
+	}
+	if ((var&8) == 8){ // hace una mascara para saber si el bit 4 esta encendido posicion 7 hamming
+		bit1+=1;		//influye en el bit de pariedad 1
+		bit2+=1;		//influye en el bit de pariedad 2
+		bit4+=1;		//influye en el bit de pariedad 4
+		hamming += 16;
+		//printf("Entre a bit 4\n");
+	}
+	if ((var&16) ==16){ // hace una mascara para saber si el bit 5 esta encendido posicion 6 hamming
+		bit2+=1;		//influye en el bit de pariedad 2
+		bit4+=1;		//influye en el bit de pariedad 2
+		hamming += 32;
+		//printf("Entre a bit 5\n");
+	}
+	if ((var&32) == 32){ // hace una mascara para saber si el bit 6 esta encendido posicion 5 hamming
+		bit1+=1;		//influye en el bit de pariedad 1
+		bit4+=1;		//influye en el bit de pariedad 2
+		hamming += 64;
+		//printf("Entre a bit 6\n");
+	}
+	if ((var&64) == 64){ // hace una mascara para saber si el bit 7 esta encendido posicion 3 hamming
+		bit2+=1;		//influye en el bit de pariedad 1
+		bit1+=1;		//influye en el bit de pariedad 2
+		hamming += 256;
+		//printf("Entre a bit 7\n");
+	}	
+
+	//printf("Resultado bits %d\n%d\n%d\n%d\n",bit1,bit2,bit4,bit8);
+	
+	if((bit1 % 2) != 0){
+		//printf("bit1 es impar \n");
+    	hamming += 1024;
+    } 
+    if((bit2 % 2) != 0){
+    	//printf("bit2 es impar \n");
+    	hamming += 512;
+    } 
+    if((bit4 % 2) != 0){
+    	//printf("bit4 es impar \n");
+    	hamming += 128;
+    } 
+    if((bit8 % 2) != 0){
+    	//printf("bit8 es impar \n");
+    	hamming += 8;
+    } 
+
+    return hamming;
+}
+// Va verificar 
+// Los primero 11 bits (7,6,5,4,3,2,1) 4 deverificacion 
+
+int hamming_VeriBits(int var) {
+
+	int bit1=0;
+	int bit2=0;
+	int bit4=0;
+	int bit8=0;
+	int error=0;
+	int hamming_verificado=0;
+
+	//printf("var&2 == 2 -> %d\n",var&1);
+	if ((var&1) == 1){ // hace una mascara para saber si el bit 1 esta encendido 
+		bit1++;		//influye en el bit de pariedad 1
+		bit2+=1;		//influye en el bit de pariedad 2
+		bit8+=1;		//influye en el bit de pariedad 8
+		hamming_verificado += 1;
+		//printf("Entre a bit 1\n");
+	}
+	//printf("BIT 11  bit2 resultante: %d\n", bit2);
+
+	//printf("var&2 == 2 -> %d\n",var&2);
+	if ((var&2) == 2){ // hace una mascara para saber si el bit 2
+		bit2++;		//influye en el bit de pariedad 2
+		bit8+=1;		//influye en el bit de pariedad 8
+		hamming_verificado += 2;
+		//printf("Entre a bit 2\n");
+	}
+	//printf("BIT 10  bit2 resultante: %d\n", bit2);
+
+	//printf("var&4 == 4 -> %d\n",var&4);
+	if ((var&4) == 4){ // hace una mascara para saber si el bit 3 esta encendido 
+		bit1+=1;		//influye en el bit de pariedad 1
+		bit8+=1;		//influye en el bit de pariedad 8
+		hamming_verificado += 4;
+		//printf("Entre a bit 3\n");
+	}
+	//printf("var&8 == 8 -> %d\n",var&8);
+	if ((var&8) == 8){ // hace una mascara para saber si el bit 4 esta encendido 
+		bit8+=1;		//influye en el bit de pariedad 8
+		//printf("Entre a bit 4\n");
+	}
+
+	//printf("var&16 == 16 -> %d\n",var&16);
+	if ((var&16) ==16){ // hace una mascara para saber si el bit 5 esta encendido 
+		bit1+=1;		//influye en el bit de pariedad 1
+		bit2+=1;		//influye en el bit de pariedad 2
+		bit4+=1;		//influye en el bit de pariedad 2
+		hamming_verificado += 8;
+		//printf("Entre a bit 5\n");
+	}
+	//printf("BIT 7  bit2 resultante: %d\n", bit2);
+
+	//printf("var&32 == 32 -> %d\n",var&32);
+	if ((var&32) == 32){ // hace una mascara para saber si el bit 6 esta encendido 
+		bit2+=1;		//influye en el bit de pariedad 2
+		bit4+=1;		//influye en el bit de pariedad 4
+		hamming_verificado += 16;
+		//printf("Entre a bit 6\n");
+	}
+	//printf("BIT 6  bit2 resultante: %d\n", bit2);
+
+	//printf("var&64 == 64 -> %d\n",var&64);
+	if ((var&64) == 64){ // hace una mascara para saber si el bit 7 esta encendido 
+		bit4+=1;		//influye en el bit de pariedad 1
+		bit1+=1;		//influye en el bit de pariedad 4
+		hamming_verificado += 32;
+		//printf("Entre a bit 7\n");
+	}
+
+	//printf("var&128 == 128 -> %d\n",var&128);	
+	if ((var&128) == 128){ // hace una mascara para saber si el bit 8 esta encendido 
+		bit4+=1;		//influye en el bit de pariedad 4
+		//printf("Entre a bit 8\n");
+	}
+	//printf("var&256 == 256 -> %d\n",var&256);
+	if ((var&256) == 256){ // hace una mascara para saber si el bit 9 esta encendido posicion 3 hamming
+		bit2+=1;		//influye en el bit de pariedad 2
+		bit1+=1;		//influye en el bit de pariedad 1
+		hamming_verificado += 64;
+		//printf("Entre a bit 9\n");
+	}
+	//printf("BIT 3  bit2 resultante: %d\n", bit2);
+
+
+	//printf("var&512 == 512 -> %d\n",var&512);
+	if ((var&512) == 512){ // hace una mascara para saber si el bit 10 esta encendido 
+		bit2+=1;		//influye en el bit de pariedad 2
+		//printf("Entre a bit 10\n");
+	}
+	//printf("BIT 2  bit2 resultante: %d\n", bit2);
+
+	//printf("var&1024 == 1024 -> %d\n",var&1024);
+	if ((var&1024) == 1024){ // hace una mascara para saber si el bit 11 esta encendido
+		bit1+=1;		//influye en el bit de pariedad 1
+		//printf("Entre a bit 11\n");
+	}
+
+	//printf("Resultado bits %d\n%d\n%d\n%d\n",bit1,bit2,bit4,bit8);
+	
+	if(((bit1 % 2) != 0) ){
+		//printf("bit1 es impar \n");
+    	error+=1;
+    } 
+    if((bit2 % 2) != 0){
+    	//printf("bit2 es impar \n");
+    	error+=2;
+    } 
+    if((bit4 % 2) != 0){
+    	//printf("bit4 es impar \n");
+    	error+=4;
+    } 
+    if((bit8 % 2) != 0){
+    	//printf("bit8 es impar \n");
+    	error+=8;
+    } 
+
+    if(error != 0){
+    	printf("Existe volteo de bit en el : %d\n",error);
+    	
+    } 
+
+    //devuelve el valor del hamming ya verificado
+    return hamming_verificado;
+}
+
+int main() {
+    unsigned int var1=0;
+    unsigned int hammingCal=0;
+    unsigned int count=0;
+    unsigned char error;
+    unsigned int bit_error=0;
+    unsigned int verificacion=0;
+
+	printf("Ingrese numero hexadecimal: ");
+    scanf(" %x", &var1);
+
+    printf("Numero en hexadecimal recibido: %x\n",var1);
+   
+   	printf("------------------------------------- \n");
+   	printf("         Calculando Hamming  \n");
+    printf("------------------------------------- \n");
+    while (var1!=0){
+    	printf("Selecciono los primero 7 bits: %x\n", var1&127);
+    	hammingCal += hamming_CalBits(var1) ;
+    	var1 = var1 >> 7;
+    	printf("var1 resultante: %x\n", var1);
+	}
+	printf("Hamming resultante: %x\n", hammingCal);
+
+	printf("------------------------------------- \n");
+   	printf("         Error a probocar  \n");
+    printf("------------------------------------- \n");
+
+	printf("Desea generar error (S/N): ");
+    scanf(" %c", &error);
+
+    while((error == 'S' | error == 's')&& count==0 ){
+    	printf("Ingrese el numero de bit que desea volvear: ");
+    	scanf(" %d", &bit_error);
+    	hammingCal = hammingCal^exponentInt(2,bit_error);
+    	printf("hamming error: %x\n", hammingCal);
+    	
+    	count = 1;
+
+    	printf("Desea generar error (S/N): ");
+    	scanf(" %c", &error);
+
+    	if(count==1){
+    		printf("No puede haber màs de un error \n");
+    		break;
+    	}
+    }
+
+	printf("------------------------------------- \n");
+   	printf("         Verificando Hamming  \n");
+    printf("------------------------------------- \n");
+	printf("hamming a veridicar: %x\n", hammingCal);
+    while (hammingCal!=0){
+    	printf("Selecciono los primero 11 bits: %x\n", hammingCal&2047);
+    	verificacion += hamming_VeriBits(hammingCal) ;
+    	hammingCal = hammingCal >> 11;
+    	printf("hamming resultante: %x\n", hammingCal);
+	}
+	printf("Hexadecimal resultante de la verificacion: %x\n", verificacion);
+
+    return 0;
+}
+ 
+
+
+
